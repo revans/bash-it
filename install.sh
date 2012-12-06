469f68214bdd0c349119e3ba53a0ded11e8cb3fa
nickl- <github@jigsoft.co.za>
2012-12-06 16:17:06 +0200

    We do insist though, you locate us at ~/.bash_it

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 BASH_IT=$(cd ${0%/*} && echo ${PWD})
 cd "${OLDPWD}"
 
+[[ "${BASH_IT}" != "${HOME}/.bash_it" ]] && cp -Rf "${BASH_IT}" "${HOME}/.bash_it"
+
 BASH_PROFILE_BAK="${HOME}/.bash_profile.bak"
 if [ -f "${BASH_PROFILE_BAK}" ]; then
   list=($(ls "${BASH_PROFILE_BAK}"*))
