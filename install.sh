a09ae4960dfc9bb6a441c02a04a6976366ccb7a5
nickl- <github@jigsoft.co.za>
2012-12-06 16:02:15 +0200

    Then use $BASH_IT
    Now that we've gone through all that trouble to define $BASH_IT lets use it then.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp "${HOME}/.bash_profile" "${HOME}/.bash_profile.bak"
 
 echo "Your original .bash_profile has been backed up to .bash_profile.bak"
 
-cp "${HOME}/.bash_it/template/bash_profile.template.bash" "${HOME}/.bash_profile"
+cp "${BASH_IT}/template/bash_profile.template.bash" "${HOME}/.bash_profile"
 
 echo "Copied the template .bash_profile into ~/.bash_profile, edit this file to customize bash-it"
 
@@ -15,7 +15,7 @@ while true; do
   read -p "Do you use Jekyll? (If you don't know what Jekyll is, answer 'n') [Y/N] " RESP
   case ${RESP} in
     [yY])
-      cp "${HOME}/.bash_it/template/jekyllconfig.template.bash" "${HOME}/.jekyllconfig"
+      cp "${BASH_IT}/template/jekyllconfig.template.bash" "${HOME}/.jekyllconfig"
       echo "Copied the template .jekyllconfig into your home directory." \
           "Edit this file to customize bash-it for using the Jekyll plugins"
       break
