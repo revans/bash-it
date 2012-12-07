3663c8e58d1e8772462748478fdb8eccfc37a502
nickl- <github@jigsoft.co.za>
2012-12-07 02:24:48 +0200

    How about a direct install with bash, curl and github
     * Use curl to retrieve the raw install script from github
     * Execute payload directly with bash -c without requiring file write
     * Detect bash execution (as apposed to terminal or script file execution) and assume that this is a direct install
     * On direct install
     * * purge ~/.bash_it if present
     * * git clone repo to ~/.bash_it
     * * continue installation as normal

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-BASH_IT=$(cd ${0%/*} && echo ${PWD})
+if [ "$0" == "bash" ]; then
+  BASH_IT="${HOME}/.bash_it"
+  [[ -d "${BASH_IT}" ]] && rm -rf "${BASH_IT}"
+  git clone http://github.com/revans/bash-it.git "${BASH_IT}"
+else
+  BASH_IT=$(cd ${0%/*} && echo ${PWD})
+fi
 
 [[ "${BASH_IT}" != "${HOME}/.bash_it" ]] && cp -Rf "${BASH_IT}" "${HOME}/.bash_it"
 
